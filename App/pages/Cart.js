d450e6a7f0d555a88050d4f700cada6b084c382c
jpha717 <140315606+jpha717@users.noreply.github.com>
2024-01-11 20:23:44 +1300

    Added back button to cart

--- App/pages/Cart.js
+++ App/pages/Cart.js
@@ -1,13 +1,38 @@
 import {useState, useEffect} from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar, useColorScheme } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, useColorScheme, BackHandler, Button, TouchableOpacity } from 'react-native';
+import { Iconify } from 'react-native-iconify'
+import { Montserrat_500Medium } from '@expo-google-fonts/montserrat';
 
 
-const Cart = ({navigation}) => {
+const Cart = ({navigation: { goBack } }) => {
     return(
-        <Text>Cart</Text>
+        <View style={styles.topBar}>
+            <TouchableOpacity onPress={() => goBack()}>
+                <Iconify icon='clarity:undo-line' size={40} color={'black'} style={styles.backIcon} />
+            </TouchableOpacity>
+            <Text style={styles.title}>Cart</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    topBar: {
+        marginTop: 76,
+        marginLeft: 15,
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    title: {
+        fontFamily: 'Montserrat_500Medium',
+        fontSize: 35,
+        flex: 1,
+        paddingLeft: 10
+    },
+    backIcon: {
+        alignItems: 'center'
+    }
+})
 
 export default Cart 
---
+++ App/pages/Search.js
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, useColorScheme, BackHandler, Button, TouchableOpacity } from 'react-native';
+import { Iconify } from 'react-native-iconify'
+import { Montserrat_500Medium } from '@expo-google-fonts/montserrat';
+import { FullWindowOverlay } from 'react-native-screens';
+
+const Search = () => {
+    return (
+        <Text>google</Text>
+    )
+}
+
+export default Search
