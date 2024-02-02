c62544217d4c09deb9d746fd9f7a2fd1d6c317e2
jpha717 <jessephan234@icloud.com>
2024-02-03 11:13:55 +1300

    Update Settings.js

--- App/pages/Settings.js
+++ App/pages/Settings.js
@@ -17,20 +17,79 @@ import {Montserrat_500Medium} from '@expo-google-fonts/montserrat'
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+const SECTIONS = [
+    {
+        header: "Account Settings",
+        items: [
+            {label: "Details"},
+            {label: "Change Username"},
+            {label: "Change Password"},
+            {label: "Payments"},
+            {label: "Disable Account"}
+        ]
+    },
+    {
+        header: "App Settings",
+        items: [
+            {label: "Change Font", value: "Montserrat"},
+            {label: "Zoom", value: "100"},
+        ]
+    },
+    {
+        header: "Legal",
+        items: [
+            {label: "Legal"},
+        ]
+    },
+        {
+        header: "Contact",
+        items: [
+            {label: "Contact Us", type: "link"},
+        ]
+    },
+        {
+        header: "Logout",
+        items: [
+            {label: "Logout"},
+        ]
+    },
+]
 
 const Settings = ({navigation}) => {
     return (
         <View style={styles.eh}>
-            <Text>Settings</Text>
+            <View style={styles.header}>
+            <Text style={styles.titles}>Settings</Text>
+            </View>
+        {SECTIONS.map(({header, items}) => (
+            <View>
+                <View>
+                    <Text>{header}</Text>
+                </View>
+                {items.map(({label, index}) => (
+                    <View>
+                        <Text>{label}</Text>
+                    </View>
+                ))}
+            </View>
+            )
+            )}
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     eh: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        color: 'red'
+        paddingVertical: 48
+    },
+    header: {
+        marginBottom: 12,
+        paddingHorizontal: 24,
+    },
+    titles: { 
+        fontSize: 32,
+        fontWeight: 500,
     }
 })
 
